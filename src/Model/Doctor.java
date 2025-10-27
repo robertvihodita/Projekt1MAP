a71e5207e784383dbf4d95e0503c45c50c28d182
xcryxopxx <cristianscorobett@gmail.com>
2025-10-27 23:40:39 +0200

    Doc

---
+++ src/Model/Doctor.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Doctor extends MedicalStaff {
+    private String licenseNumber;
+
+    public Doctor() { super(); }
+    public Doctor(String id, String name, String departmentId, String licenseNumber) {
+        super(id, name, departmentId);
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicenseNumber() { return licenseNumber; }
+    public void setLicenseNumber(String licenseNumber) { this.licenseNumber = licenseNumber; }
+}
+
