afc0ba6b9cb84b44916e3ba77099f606d906e179
xcryxopxx <cristianscorobett@gmail.com>
2025-10-27 23:37:45 +0200

    App

---
+++ src/Model/Appointment.java
@@ -0,0 +1,35 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private String id;
+    private String departmentId;
+    private String patientId;
+    private LocalDateTime admissionDate;
+    private String status; // "Active"/"Completed"
+    private List<String> medicalStaff = new ArrayList<>();
+
+    public Appointment() {}
+    public Appointment(String id, String departmentId, String patientId, LocalDateTime admissionDate, String status) {
+        this.id = id;
+        this.departmentId = departmentId;
+        this.patientId = patientId;
+        this.admissionDate = admissionDate;
+        this.status = status;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getDepartmentId() { return departmentId; }
+    public void setDepartmentId(String departmentId) { this.departmentId = departmentId; }
+    public String getPatientId() { return patientId; }
+    public void setPatientId(String patientId) { this.patientId = patientId; }
+    public LocalDateTime getAdmissionDate() { return admissionDate; }
+    public void setAdmissionDate(LocalDateTime admissionDate) { this.admissionDate = admissionDate; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+    public List<String> getMedicalStaff() { return medicalStaff; }
+}
