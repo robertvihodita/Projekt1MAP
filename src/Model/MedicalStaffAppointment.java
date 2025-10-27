b5cf66235553cee0d6a999b12015479a36f3fac9
xcryxopxx <cristianscorobett@gmail.com>
2025-10-27 23:43:58 +0200

    MedStaff mod

---
+++ src/Model/MedicalStaffAppointment.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class MedicalStaffAppointment {
+    private String id;
+    private String appointmentId;
+    private String medicalStaffId;
+
+    public MedicalStaffAppointment() {}
+    public MedicalStaffAppointment(String id, String appointmentId, String medicalStaffId) {
+        this.id = id;
+        this.appointmentId = appointmentId;
+        this.medicalStaffId = medicalStaffId;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getAppointmentId() { return appointmentId; }
+    public void setAppointmentId(String appointmentId) { this.appointmentId = appointmentId; }
+    public String getMedicalStaffId() { return medicalStaffId; }
+    public void setMedicalStaffId(String medicalStaffId) { this.medicalStaffId = medicalStaffId; }
+}
