4a5b402fa6885143c090d9fae0799a3c2cb92217
xcryxopxx <cristianscorobett@gmail.com>
2025-10-27 23:41:47 +0200

    Nurse

---
+++ src/Model/Nurse.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Nurse extends MedicalStaff {
+    private String qualificationLevel;
+
+    public Nurse() { super(); }
+    public Nurse(String id, String name, String departmentId, String qualificationLevel) {
+        super(id, name, departmentId);
+        this.qualificationLevel = qualificationLevel;
+    }
+
+    public String getQualificationLevel() { return qualificationLevel; }
+    public void setQualificationLevel(String qualificationLevel) { this.qualificationLevel = qualificationLevel; }
+}
