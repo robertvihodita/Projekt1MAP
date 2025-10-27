e5e22b1aee34b8c8f7dc4a85aa44ebf0463add16
xcryxopxx <cristianscorobett@gmail.com>
2025-10-27 23:39:26 +0200

    Med

---
+++ src/Model/MedicalStaff.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MedicalStaff {
+    private String id;
+    private String name;
+    private List<String> appointments = new ArrayList<>();
+    private String departmentId;
+
+    public MedicalStaff() {}
+    public MedicalStaff(String id, String name, String departmentId) {
+        this.id = id;
+        this.name = name;
+        this.departmentId = departmentId;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public List<String> getAppointments() { return appointments; }
+    public String getDepartmentId() { return departmentId; }
+    public void setDepartmentId(String departmentId) { this.departmentId = departmentId; }
+}
