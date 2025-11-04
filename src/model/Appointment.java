b2e2ca9fd8ff5e54b635c13390075f5ef78711e1
xcryxopxx <cristianscorobett@gmail.com>
2025-11-04 04:03:32 +0200

    mod

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,18 +1,25 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDateTime;
 
 public class Appointment {
     private String id;
     private String departmentId;
     private String patientId;
     private LocalDateTime admissionDate;
-    private String status; // "Active"/"Completed" // enum & la fel pt toate de tipul asta ; datele cu localdate
-    private List<String> medicalStaff = new ArrayList<>();
+    private String status;
+    private List<String> medicalStaff = new ArrayList();
 
-    public Appointment() {}
+    public Appointment() {
+    }
+
     public Appointment(String id, String departmentId, String patientId, LocalDateTime admissionDate, String status) {
         this.id = id;
         this.departmentId = departmentId;
@@ -21,15 +28,47 @@ public class Appointment {
         this.status = status;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getDepartmentId() { return departmentId; }
-    public void setDepartmentId(String departmentId) { this.departmentId = departmentId; }
-    public String getPatientId() { return patientId; }
-    public void setPatientId(String patientId) { this.patientId = patientId; }
-    public LocalDateTime getAdmissionDate() { return admissionDate; }
-    public void setAdmissionDate(LocalDateTime admissionDate) { this.admissionDate = admissionDate; }
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-    public List<String> getMedicalStaff() { return medicalStaff; }
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepartmentId() {
+        return this.departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getPatientId() {
+        return this.patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public LocalDateTime getAdmissionDate() {
+        return this.admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDateTime admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getMedicalStaff() {
+        return this.medicalStaff;
+    }
 }
--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package model;
 
 import java.util.ArrayList;
@@ -7,22 +12,47 @@ public class Hospital {
     private String id;
     private String name;
     private String city;
-    private List<String> departments = new ArrayList<>();
-    private List<String> rooms = new ArrayList<>();
+    private List<String> departments = new ArrayList();
+    private List<String> rooms = new ArrayList();
 
-    public Hospital() {}
+    public Hospital() {
+    }
+
     public Hospital(String id, String name, String city) {
         this.id = id;
         this.name = name;
         this.city = city;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
-    public List<String> getDepartments() { return departments; }
-    public List<String> getRooms() { return rooms; }
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getDepartments() {
+        return this.departments;
+    }
+
+    public List<String> getRooms() {
+        return this.rooms;
+    }
 }
