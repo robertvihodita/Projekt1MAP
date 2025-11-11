b3a43c73d347ab601369270b7c2da30bc4d099b4
xcryxopxx <cristianscorobett@gmail.com>
2025-11-11 05:18:02 +0200

    spring

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"controller", "model", "repository", "service"})
 public class Application {
 
     public static void main(String[] args) {
